286eb0706f6d566898c13121ca5e3bf7990d5b51
Igor Nedovic <igornedovic@gmail.com>
2021-01-31 19:20:48 +0100

    Initial commit

---
+++ EPOSProjektni/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using EPOSProjektni.Areas.Identity.Data;
+using EPOSProjektni.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(EPOSProjektni.Areas.Identity.IdentityHostingStartup))]
+namespace EPOSProjektni.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AuthDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AuthDbContextConnection")));
+
+                services.AddDefaultIdentity<User>(options => {
+                    options.SignIn.RequireConfirmedAccount = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                 })
+                    .AddEntityFrameworkStores<AuthDbContext>();
+            });
+        }
+    }
+}
